bea88488d87ded2322c8fcadba46f2d902cb09e6
Relton Fidelis ME <contato.reltonfidelis@gmail.com>
2020-03-12 15:45:21 -0300

    meu primeiro component

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+    <p>Meu primeiro component criado com Angular 8</p>
+    `
+})
+export class MeuPrimenroComponent{ }
